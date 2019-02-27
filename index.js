e6c1c5b62891ab557775298e4e28c8e42a8770cb
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-27 17:32:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function showResults(json) {
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  const token = '75665f68a1b9e1840952847865862b8c9762f002' 
+  const token = '75665f68a1b9e1840952847865862b8c9762f002'
 }
 
 function getToken() {
