235a3879071626b25093515137d81199b59b5220
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-27 17:36:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ function showResults(json) {
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab' ;
   const token = getToken() ;
+  fetch('https://api.github.com/repos/:your_ghname/:your_repo/commits/:sha/comments', {
+    method: 'POST',
+    body: JSON.stringify(postData),
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => console.log(res));
 
 }
 
